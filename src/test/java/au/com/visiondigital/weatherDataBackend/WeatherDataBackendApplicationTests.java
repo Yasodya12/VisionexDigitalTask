0dfe2b6762f34cf1236124c266ce9c1a1eb65029
Yasodya12 <rishikayasodya12@gmail.com>
2024-12-02 12:07:48 +0530

    first commit

---
+++ src/test/java/au/com/visiondigital/weatherDataBackend/WeatherDataBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.visiondigital.weatherDataBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDataBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
